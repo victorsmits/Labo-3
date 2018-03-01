d79eed3ed79cbc93460719e3043c7e2ddfc7f50b
victorsmits <victor.smits@student.vinci.be>
2018-03-01 11:10:45 +0100

    random dice

--- 1000des.py
+++ 1000des.py
@@ -1,8 +1,25 @@
+import threading as th
+from random import randint
 
-import multiprocessing as mp
-import random
-import threading
+counter = 0
+list = []
+lock = th.Lock()
+result = 0
 
 
-def random_dice(n):
-    while i <= n:
+def compute(n):
+    global counter
+    global result
+    for i in range(0, n):
+        with lock:
+            counter = (randint(1, 6))
+            list.append(counter)
+    result += sum(list) / len(list)
+
+
+threads = [th.Thread(target=compute, args=(1000,))]
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
+print(result)

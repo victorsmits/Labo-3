58f3b5138af2623823a7471338303d223b10eeaf
victorsmits <victor.smits@student.vinci.be>
2018-03-01 09:19:40 +0100

    Chat V1.0

--- Chat_V1.0.py
+++ Chat_V1.0.py
@@ -1,9 +1,40 @@
 
 import socket
 
-class Chat ():
-    def __init__ (self , host = socket . gethostname () , port =5000) :
-         s = socket . socket ( type = socket . SOCK_DGRAM )
-         s. settimeout (0.5)
-         s. bind (( host , port ))
-         self . __s = s
+
+class Chat():
+
+    def __init__(self, host=socket . gethostname(), port=5000):
+        s = socket.socket(type=socket . SOCK_DGRAM)
+        s.settimeout(0.5)
+        s.bind((host, ))
+        self.__s = s
+
+    def _exit(self):
+        self.__running = False
+        self.__address = None
+        self.__s.close()
+
+    def _quit(self):
+        self.__address = None
+
+    def _join(self,param):
+        tokens = param.splot(' ')
+        if len(tokens) == 2:
+            self.__address = (socket.gethostbyaddr(tokens[0])[0], int(tokens[1]))
+
+    def _send(self,param):
+        if self.__address is not None:
+            message = param.encode()
+            totalsent = 0
+            while totalsent < len(message):
+                sent = self.__s.sendto(message[totalsent:], self.__address)
+                totalsent += sent
+
+    def _receive(self):
+        while self.__running:
+            try:
+                data, address = self.__s.recvfrom(1024)
+                print(data.encode())
+            except socket.timeout:
+                pass

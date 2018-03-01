619b8e2539c2408d8248e1fee985da7264ea3d3f
victorsmits <victor.smits@student.vinci.be>
2018-03-01 09:46:38 +0100

    Chat V1.0 update

--- Chat_V1.0.py
+++ Chat_V1.0.py
@@ -1,16 +1,44 @@
+#!/usr/bin/env python3
+# chat.py
+# author: Sébastien Combéfis
+# version: February 15, 2016
 
 import socket
 import sys
 import threading
 
 
-class Chat():
-
-    def __init__(self, host=socket . gethostname(), port=5000):
-        s = socket.socket(type=socket . SOCK_DGRAM)
+class Chat:
+    def __init__(self, host=socket.gethostname(), port=5000):
+        s = socket.socket(type=socket.SOCK_DGRAM)
         s.settimeout(0.5)
         s.bind((host, port))
         self.__s = s
+        print('Écoute sur {}:{}'.format(host, port))
+
+    def run(self):
+        handlers = {
+            '/exit': self._exit,
+            '/quit': self._quit,
+            '/join': self._join,
+            '/send': self._send
+        }
+        self.__running = True
+        self.__address = None
+        threading.Thread(target=self._receive).start()
+        while self.__running:
+            line = sys.stdin.readline().rstrip() + ' '
+            # Extract the command and the param
+            command = line[:line.index(' ')]
+            param = line[line.index(' ') + 1:].rstrip()
+            # Call the command handler
+            if command in handlers:
+                try:
+                    handlers[command]() if param == '' else handlers[command](param)
+                except:
+                    print("Erreur lors de l'exécution de la commande.")
+            else:
+                print('Command inconnue:', command)
 
     def _exit(self):
         self.__running = False
@@ -20,45 +48,39 @@ class Chat():
     def _quit(self):
         self.__address = None
 
-    def _join(self,param):
-        tokens = param.splot(' ')
+    def _join(self, param):
+        tokens = param.split(' ')
         if len(tokens) == 2:
-            self.__address = (socket.gethostbyaddr(tokens[0])[0], int(tokens[1]))
+            try:
+                self.__address = (socket.gethostbyaddr(tokens[0])[0], int(tokens[1]))
+                print('Connecté à {}:{}'.format(*self.__address))
+            except OSError:
+                print("Erreur lors de l'envoi du message.")
 
-    def _send(self,param):
+    def _send(self, param):
         if self.__address is not None:
-            message = param.encode()
-            totalsent = 0
-            while totalsent < len(message):
-                sent = self.__s.sendto(message[totalsent:], self.__address)
-                totalsent += sent
+            try:
+                message = param.encode()
+                totalsent = 0
+                while totalsent < len(message):
+                    sent = self.__s.sendto(message[totalsent:], self.__address)
+                    totalsent += sent
+            except OSError:
+                print('Erreur lors de la réception du message.')
 
     def _receive(self):
         while self.__running:
             try:
                 data, address = self.__s.recvfrom(1024)
-                print(data.encode())
+                print(data.decode())
             except socket.timeout:
                 pass
+            except OSError:
+                return
 
-    def run(self):
-        handlers = {
-            '/exit': self._exit,
-            '/join': self._join,
-            '/quit': self._quit,
-            '/send': self._send,
-        }
 
-        self.__running = True
-        self.__address = None
-
-        threading.Thread(target=self._receive).start()
-
-        while self.__running:
-            line = sys.stdin.readline().rstrip() + ' '
-            command = line[:line.index(' ')]
-            param = line[line.index(' ')+1:].rstrip()
-            if command in handlers:
-                handlers[command]() if param == ' ' else handlers[command](param)
-            else:
-                print('Uknown command: ', command)
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        Chat(sys.argv[1], int(sys.argv[2])).run()
+    else:
+        Chat().run()
